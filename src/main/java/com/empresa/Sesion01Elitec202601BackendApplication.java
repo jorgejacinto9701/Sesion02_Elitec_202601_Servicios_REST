92d336ffde0c1c9f01c2659c488a9210ac456346
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2026-03-11 18:59:08 -0500

    primer commit

---
+++ src/main/java/com/empresa/Sesion01Elitec202601BackendApplication.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion01Elitec202601BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion01Elitec202601BackendApplication.class, args);
+	}
+
+}
